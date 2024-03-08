c5851f71d1c0d437d4134cef10cfff03ee472626
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-08 11:01:01 +0530

    Forms with structural directives - ngIF - ngFor

--- src/app/users/signup/signup.component.ts
+++ src/app/users/signup/signup.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Region } from '../region';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
+  regions: Region[] = [
+    { code: 'AP', name: 'Asia Pacific' },
+    { code: 'NA', name: 'North America' },
+    { code: 'SA', name: 'South America' },
+    { code: 'AR', name: 'Africa' },
+    { code: 'EU', name: 'Europe' },
+    { code: 'AU', name: 'Australia' },
+  ]
 }
