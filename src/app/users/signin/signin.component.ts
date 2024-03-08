ea3f05b314bf8ddf4ac5ffe7db4d03c0df388974
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-08 11:23:24 +0530

    Template driven forms and template variables

--- src/app/users/signin/signin.component.ts
+++ src/app/users/signin/signin.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Signin } from '../signin';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+
+  userInfo: Signin = new Signin('', '')
+
+  signInSubmit() {
+    console.log(this.userInfo)
+  }
 
 }
