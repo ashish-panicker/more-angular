6b7313a87aec474a26252597432cc72bf2452103
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-08 12:13:17 +0530

    Template driven from validation

--- src/app/users/signin/signin.component.ts
+++ src/app/users/signin/signin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Signin } from '../signin';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, NgClass],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
@@ -13,8 +14,8 @@ export class SigninComponent {
 
   userInfo: Signin = new Signin('', '')
 
-  signInSubmit() {
-    console.log(this.userInfo)
+  signInSubmit(form:NgForm) {
+    console.log(form.value)
   }
 
 }
